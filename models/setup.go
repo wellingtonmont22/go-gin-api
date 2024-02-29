cdc1d7e9d40002e949b54c3b48d7dd1483c8b3c9
Wellington Monteiro <wellington.m.alves@hotmail.com>
2024-02-29 01:35:11 -0300

    ajustando criação do banco de dados.

--- models/setup.go
+++ models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("app.sqlite"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
